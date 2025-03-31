a9eca74aad5d5dd82885fe28dc0d0d63704a0ad3
Banegas , Agustina <133693422+BanegasAgustina@users.noreply.github.com>
2025-03-31 19:17:14 -0300

    Add files via upload

---
+++ punto5.py
@@ -0,0 +1,8 @@
+n1=int(input("ingrese numero:"))
+n2=int(input("ingrese otro numero:"))
+n3=int(input("ingrese otro numero:"))
+
+if n1 < 10 and n2 <10 and n3 <10:
+    print("todos los numeros son menores a 10")
+else:
+    print("no todos son menores a 10")
