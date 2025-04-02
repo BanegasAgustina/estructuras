36ef7470c63010dd0e1c88c4c818bf410a08ec2b
Banegas , Agustina <133693422+BanegasAgustina@users.noreply.github.com>
2025-04-02 12:01:21 -0300

    Add files via upload

---
+++ punto10.py
@@ -0,0 +1,10 @@
+b= int(input("Ingrese primer valor:"))
+a= int(input("Ingrese segundo valor:"))
+c= int(input("Ingrese segundo valor:"))
+
+if a>b and a>c:
+    print("el mayor es",a)
+elif  b>a and b>c:
+    print("el mayor es",b)
+elif c>a and c>b:
+    print("el mayor es",c)
