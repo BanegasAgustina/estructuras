dab1c3d78c807a119ac583c3060f09da98a2ee55
Banegas , Agustina <133693422+BanegasAgustina@users.noreply.github.com>
2025-04-02 12:21:21 -0300

    Add files via upload

---
+++ punto11.py
@@ -0,0 +1,8 @@
+n= int(input("Ingrese segundo valor:"))
+
+if n<0:
+    print("El numero ",n," es negativo")
+elif n>0:
+     print("El numero ",n," es positivo")
+elif n==0:
+      print("El numero ",n," es nulo")
